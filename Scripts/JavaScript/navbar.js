9caeedc6c2a9d3c24050e7a31dc66dc1651cc87a
eazycheezy <lucvankessel123@gmail.com>
2025-04-16 14:20:09 +0200

    account creation page created

--- Scripts/JavaScript/navbar.js
+++ Scripts/JavaScript/navbar.js
@@ -1,12 +1,17 @@
 // Save this as navbar.js and include it in each HTML page
 function loadNavbar() {
-  fetch('navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      const navbarContainer = document.createElement('div');
-      navbarContainer.innerHTML = data;
-      document.body.insertBefore(navbarContainer, document.body.firstChild);
-    });
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            const navbarContainer = document.createElement('div');
+            navbarContainer.innerHTML = data;
+            document.body.insertBefore(navbarContainer, document.body.firstChild);
+        });
+}
+
+function toggleNavbar() {
+    const navBar = document.querySelector('.navBar');
+    navBar.classList.toggle('expanded');
 }
 
 document.addEventListener('DOMContentLoaded', loadNavbar);
